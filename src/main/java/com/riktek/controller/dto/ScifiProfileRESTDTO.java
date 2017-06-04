0265a60fbf06902b4db65b8372a5637f2de0d851
rikitzkowitz128 <cpm128@yahoo.com>
2017-06-04 11:39:13 -0700

    hello

---
+++ src/main/java/com/riktek/controller/dto/ScifiProfileRESTDTO.java
@@ -0,0 +1,18 @@
+package com.riktek.controller.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class ScifiProfileRESTDTO
+{
+	@Getter private String id;
+	@Getter @Setter private int entityType;
+	@Getter @Setter private String designation;
+	
+	
+	public void update(int t, String s)
+	{
+		entityType = t;
+		designation = s;
+	}
+}
