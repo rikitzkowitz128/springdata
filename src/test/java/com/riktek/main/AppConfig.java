888e3a09b44447b172e2448e365590e94d1d6ccd
rikitzkowitz128 <cpm128@yahoo.com>
2017-06-03 18:54:35 -0700

    CRUD

---
+++ src/test/java/com/riktek/main/AppConfig.java
@@ -0,0 +1,15 @@
+package com.riktek.main;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({"com.riktek.controller","com.riktek.service"})
+@EnableMongoRepositories("com.riktek.repository")
+public class AppConfig 
+{
+}
